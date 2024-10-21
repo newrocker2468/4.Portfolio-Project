0111850d544422af0e551f82adcbcda50e55a2a1
newrocker2468 <newrocker2468@gmail.com>
2024-10-21 22:31:46 +0530

    refactor: Remove PDF download route from server index

--- server/index.js
+++ server/index.js
@@ -2,10 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.get("/download", (req, res) => {
-  const file = path.join(__dirname, "docs", "Resume.pdf");
-  res.download(file);
-});
+
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
