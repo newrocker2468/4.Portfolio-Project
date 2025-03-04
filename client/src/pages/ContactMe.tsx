b94343a81ed01e55700b968dbadfb68b4c36f839
newrocker2468 <newrocker2468@gmail.com>
2025-03-04 18:13:58 +1100

    feat: add Formik for form management; update ContactMe page with improved input placeholders and additional instructions

--- client/src/pages/ContactMe.tsx
+++ client/src/pages/ContactMe.tsx
@@ -50,17 +50,20 @@ const ContactMe = React.forwardRef<HTMLDivElement, HomeProps>(() => {
         />
       )}
       <h1 className='text-center text-3xl'>Wanna Get In Touch ? </h1>
+      <p className='text-center text-lg mt-4'>
+        Fill the form below to get in touch with me.
+      </p>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-[3rem]'>
         <div className='flex md:justify-end justify-center col-span-2 md:col-span-1'>
           <div className='w-1/2 grid grid-cols-1'>
             <span className='w-full text-start my-[0.3rem]'>Name :</span>
-            <Input type='text' placeholder='Name' />
+            <Input type='text' placeholder='Enter yours or companys' />
           </div>
         </div>
         <div className='flex md:justify-start justify-center col-span-2 md:col-span-1'>
           <div className='w-1/2 grid grid-cols-1'>
             <span className='w-full text-start my-[0.3rem]'>Email :</span>
-            <Input type='email' placeholder='Email' />
+            <Input type='email' placeholder='Your Mail ID' />
           </div>
         </div>
         <div className='flex justify-center col-span-2'>
@@ -72,17 +75,27 @@ const ContactMe = React.forwardRef<HTMLDivElement, HomeProps>(() => {
         <div className='flex justify-center col-span-2'>
           <div className='w-1/2 grid grid-cols-1'>
             <span className='w-full text-start my-[0.3rem]'>Message :</span>
-            <Textarea className='w-full' />
+            <Textarea className='w-full' placeholder="Enter your message here "/>
           </div>
         </div>
 
         <div className='flex justify-center col-span-2'>
           <button
-            className={`px-4 py-2 ${effectiveTheme == "dark" ? "bg-white text-black" :" bg-black text-white"}  rounded-lg w-1/2 `}
+            className={`px-4 py-2 ${
+              effectiveTheme == "dark"
+                ? "bg-white text-black"
+                : " bg-black text-white"
+            }  rounded-lg w-1/2 dark:hover:bg-slate-200 hover:bg-[#515151]`}
           >
             Send
           </button>
         </div>
+      </div>
+      <div>
+        <h2 className='text-center my-4'>
+          If you prefer not to fill out the form, no problem at all. You can
+          reach me directly via email at hello@jaskaran.dev.
+        </h2>
       </div>
     </div>
   );
